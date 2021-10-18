ef5889d2725cfdec5d8b57af29c42a5f9af44e3b
Anjali Mishra <anjalimarch24@gmail.com>
2021-10-18 22:59:52 +0530

    second commit

--- test/specs/elements/loginTest.js
+++ test/specs/elements/loginTest.js
@@ -1,4 +1,5 @@
 var loginPage=require('../../pageObjects/LoginPage')
+const expectChai=require('chai').expect
 
 
 describe('My Login application', function() {
@@ -7,12 +8,15 @@ describe('My Login application', function() {
         await loginPage.launchUrl('https://devopssaas-qa.apps.tax/')
  
        
-       await  browser.pause(5000);
+         await  browser.pause(5000);
        
-       await browser.maximizeWindow();
+         await browser.maximizeWindow();
 
 
-       await loginPage.Login();
+         await loginPage.Login();
+         const isDisplayed = await $("#relatedInformationViewToolbar .ng-binding").isDisplayed()
+         await  browser.pause(5000);
+         expectChai(isDisplayed).to.equal(true); // Chai assertion
 
    
     
